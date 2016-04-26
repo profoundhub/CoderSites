42227dcc017c14418d9562c4e8f91618db98eb92
Daniel Lim <campsites@mobilegta.com>
2016-04-26 20:45:03 +0000

    update data entry components

--- db/migrate/20160426030104_create_posts.rb
+++ db/migrate/20160426030104_create_posts.rb
@@ -3,6 +3,8 @@ class CreatePosts < ActiveRecord::Migration
     create_table :posts do |t|
       t.string :title
       t.text :body
+      t.string :author
+      t.text :url
 
       t.timestamps
     end
