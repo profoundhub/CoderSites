5201e57c02d4cf0f07e7c66a7c1f662226632a62
Daniel Lim <campsites@mobilegta.com>
2016-04-26 19:18:19 +0000

    recompile app

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,20 +2,12 @@
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-<<<<<<< HEAD
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-=======
 // or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
->>>>>>> 39e8dc1b1707b0335ff9f53b1ab9fac4a0cc1e1f
 //
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file.
 //
-<<<<<<< HEAD
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-=======
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
->>>>>>> 39e8dc1b1707b0335ff9f53b1ab9fac4a0cc1e1f
 // about supported directives.
 //
 //= require jquery
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,12 +4,9 @@ require 'rails/test_help'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-<<<<<<< HEAD
-=======
   #
   # Note: You'll currently still have to declare fixtures explicitly in integration tests
   # -- they do not yet inherit this setting
->>>>>>> 39e8dc1b1707b0335ff9f53b1ab9fac4a0cc1e1f
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
