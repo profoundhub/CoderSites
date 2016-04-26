871d55a61d1c9efcfe21add39afc959d2e59f33c
Daniel Lim <campsites@mobilegta.com>
2016-04-26 19:46:27 +0000

    Added about page view, action, route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   resources :posts
   root 'posts#index'
   root 'pages#home'
-
+  get '/about' => 'pages#about'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
