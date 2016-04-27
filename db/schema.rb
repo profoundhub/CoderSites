d8cb3d00fcf85bd9a63d40e4b02ccda7091b0882
Daniel Lim <campsites@mobilegta.com>
2016-04-27 01:21:08 +0000

    re up down

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 20160426223329) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
-    t.text     "description"
+    t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "author"
