1589f3bdb234499775e96b33a5feb4ffa144d71b
Newton <anthpaul0@gmail.com>
2016-04-27 13:55:31 -0500

    Change body to description and finish select in form

---
+++ app/helpers/descriptions_helper.rb
@@ -0,0 +1,13 @@
+module DescriptionsHelper
+
+  def description_array
+    [
+      'Tribute Page',
+      'Personal Portfolio Webpage',
+      'Build a Random Quote Machine',
+      'Show the Local Weather',
+      'Build a Wikipedia Viewer',
+      'Use the Twitch.tv JSON API'
+    ]
+  end
+end
