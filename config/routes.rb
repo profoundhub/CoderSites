f3cd9c7bccb3b1bb73210d1afcf53755edf79071
Daniel Lim <campsites@mobilegta.com>
2016-04-27 03:16:37 +0000

    Heroku is back online, we are getting it ready to go live

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   # resources :contacts
   resources :posts
   get '/about' => 'pages#about'
-  root 'pages#home'
-  
+  root 'posts#index'
+  # root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
