8dfd108735f6bd4f13f5dddc41cbcca78617b65f
Daniel Lim <campsites@mobilegta.com>
2016-04-27 02:07:15 +0000

    restore Heroku 004

--- db/migrate/20160426030104_create_posts.rb
+++ db/migrate/20160426030104_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
       t.string :title
-      t.text :description
+      t.text :body
       t.string :author
       t.string :url
       t.timestamps
